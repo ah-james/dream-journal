420fd60022f84d3322e8d53a68e226042392edd0
ah-james <11ahj11@gmail.com>
2021-02-14 18:01:55 -0500

    Finding Elements by ID in JS File

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,7 @@
-const 
+// get all of the elements
+const dreamTitle = document.getElementById('title')
+const dreamDate = document.getElementById('date')
+const dreamDesc = document.getElementById('description')
+const dreamType = document.getElementById('type')
+const submit = document.getElementById('submit')
+
