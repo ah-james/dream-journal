88c69a51bde4c55ad2b20ca973b45e20a59185c8
ah-james <11ahj11@gmail.com>
2021-02-14 19:34:25 -0500

    Adding Rails Backend

--- frontend/index.js
+++ frontend/index.js
@@ -5,3 +5,8 @@ const dreamDesc = document.getElementById('description')
 const dreamType = document.getElementById('type')
 const submit = document.getElementById('submit')
 
+submit.addEventListener("click", submitForm) // event listener on submit button
+
+const submitForm = function() {
+
+}
