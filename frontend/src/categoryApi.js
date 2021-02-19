bdc0848ccb21b50b12ef4f26012c8510884c4240
ah-james <11ahj11@gmail.com>
2021-02-19 11:44:34 -0500

    Categories Fetch Request

--- frontend/src/categoryApi.js
+++ frontend/src/categoryApi.js
@@ -1,3 +1,17 @@
 class CategoryApi {
     
+    constructor(port) {
+        this.baseUrl = `${port}/categories`
+    }
+
+    getCategories() {
+        fetch(this.baseUrl)
+        .then(resp => resp.json())
+        .then(json => {
+            json["data"].forEach(element => {
+                const i = new Dream({id: element.id, ...element.attributes})
+                i.addToDropdowns()
+            })
+        })
+    }
 }
