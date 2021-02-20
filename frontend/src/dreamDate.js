7c4e7153d8b93ba8ae250b6e1f0993c107e601cd
ah-james <11ahj11@gmail.com>
2021-02-19 20:26:13 -0500

    Showing Dream Dates on Page

--- frontend/src/dreamDate.js
+++ frontend/src/dreamDate.js
@@ -1,3 +1,15 @@
-class Date {
-    
+class DreamDate {
+
+    constructor({id, date}) {
+        this.id = id
+        this.date = date
+    }
+
+    addToFilterDropdown() {
+        const filterDropdown = document.getElementById('dream-date-sort')
+        const option = document.createElement('option')
+        option.value = this.id
+        option.innerText = this.date
+        filterDropdown.append(option)
+    }
 }
--- frontend/src/dreamDateApi.js
+++ frontend/src/dreamDateApi.js
@@ -1,3 +1,17 @@
-class DateApi {
+class DreamDateApi {
     
+    constructor(port) {
+        this.baseUrl = `${port}/dream_dates`
+    }
+
+    getDreamDates() {
+        fetch(this.baseUrl)
+        .then(resp => resp.json())
+        .then(json => {
+            json["data"].forEach(element => {
+                const i = new DreamDate({id: element.id, ...element.attributes})
+                i.addToFilterDropdown()
+            })
+        })
+    }
 }
