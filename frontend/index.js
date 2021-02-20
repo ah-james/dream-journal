3ed8e4c37e2eb79fbcedffe7151fc471767936b9
ah-james <11ahj11@gmail.com>
2021-02-19 20:26:53 -0500

    Showing Dream Dates on Page

--- frontend/index.js
+++ frontend/index.js
@@ -2,6 +2,7 @@
 const port = 'http://localhost:3000'
 const dreamApi = new DreamApi(port)
 const categoryApi = new CategoryApi(port)
+const dreamDateApi = new DreamDateApi(port)
 const dreamList = document.getElementById('dreams-container')
 const dreamTitle = document.getElementById('title')
 const dreamDate = document.getElementById('date')
@@ -19,3 +20,4 @@ function submitForm(event) {
 
 dreamApi.getDreams()
 categoryApi.getCategories()
+dreamDateApi.getDreamDates()
