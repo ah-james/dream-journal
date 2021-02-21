341e5f43f4f49721e88b38738cf3ea0be2854117
ah-james <11ahj11@gmail.com>
2021-02-20 23:46:19 -0500

    Adding Listeners on Reset and Filter

--- frontend/index.js
+++ frontend/index.js
@@ -2,6 +2,7 @@
 const port = 'http://localhost:3000'
 const dreamApi = new DreamApi(port)
 const categoryApi = new CategoryApi(port)
+const form = document.getElementById('dream-form')
 const dreamList = document.getElementById('dreams-container')
 const dreamTitle = document.getElementById('title')
 const dreamDate = document.getElementById('date')
@@ -10,6 +11,7 @@ const dreamType = document.getElementById('type')
 const dreamDropdown = document.getElementById('create-dropdown')
 const submit = document.getElementById('submit')
 const reset = document.getElementById('reset')
+const filter = document.getElementById('filter')
 
 submit.addEventListener("click", submitForm) // event listener on submit button
 
@@ -18,6 +20,18 @@ submit.addEventListener("click", submitForm) // event listener on submit button
 function submitForm(event) {
     event.preventDefault()
     dreamApi.addDream()
+    form.reset()
+}
+
+filter.addEventListener('click', filterDreams)
+reset.addEventListener('click', resetFilter)
+
+function filterDreams() {
+
+}
+
+function resetFilter() {
+
 }
 
 dreamApi.getDreams()
