48e6e98d9c96a107a8dadc35d33e08adf47074ac
ah-james <11ahj11@gmail.com>
2021-09-01 13:34:18 -0400

    Temporarily Removing Broken Favorite and Edit Options Due to Bugs

--- frontend/src/dream.js
+++ frontend/src/dream.js
@@ -44,7 +44,6 @@ class Dream {
         this.title = this.element.querySelector(".edit-title").value
         this.description = this.element.querySelector(".edit-description").value
         this.date = this.element.querySelector(".edit-date").value
-        
         dreamApi.editDream(this)
     }
 
@@ -90,12 +89,12 @@ class Dream {
                 <h5 class="card-title">${this.title}</h5>
                 <h6 class="card-subtitle mb-2 text-muted">${this.date}</h6>                
                 <p class="card-text">${this.description}</p>
-                <button  class="btn btn-outline-dark" data-id="${this.id}">Favorite</button>
-                <button class="btn btn-outline-secondary" data-id="${this.id}">Edit</button>
                 <button class="btn btn-outline-danger" data-id="${this.id}">Delete</button>
             </div>
         </div>
         `
+        // <button class="btn btn-outline-secondary" data-id="${this.id}">Edit</button>
+        // <button  class="btn btn-outline-dark" data-id="${this.id}">Favorite</button>
         return this.element
     }
 
